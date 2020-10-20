4bd8e611ad07c40a66ee7f93e4a489cac0467aab
samialam8 <sami.alam10@gmail.com>
2020-10-19 19:57:54 -0600

    Structure with dot operator

---
+++ Structmodel/main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 19, 2020
+ *      Author: Mohammad Sami Alam
+ */
+
+
+#include <stdio.h>
+#include <stdint.h>
+
+struct CarModel
+	{
+		unsigned int carNumber;
+		uint32_t carPrice;
+		uint16_t carMaxSpeed;
+		float carWeight;
+	};
+
+int main(void)
+{
+
+    struct CarModel carChevy = {2021, 15000, 220, 1330};
+
+    carChevy.carNumber = 1000;//to change data given
+
+   printf("Deatils of car:\n");
+   printf("carNumber = %u\n", carChevy.carNumber);
+   printf("carPrice = %u\n", carChevy.carPrice);
+   printf("carMaxSpeed = %u\n", carChevy.carMaxSpeed);
+   printf("carWeight = %lf\n", carChevy.carWeight);
+    return(0);
+}
